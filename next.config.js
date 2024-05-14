46fa08538e3e41fd5a4633835ecd7717071a3e75
narustudio <slackjawed12@gmail.com>
2024-05-14 22:19:05 +0900

    remove export command

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-const { withContentlayer } = require("next-contentlayer");
+const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  output: 'export',
 };
 
 module.exports = withContentlayer(nextConfig);
